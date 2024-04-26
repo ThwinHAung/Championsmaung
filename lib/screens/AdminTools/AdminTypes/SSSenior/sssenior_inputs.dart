29fdb92244c857387ef142cae2684b4225f6d9fc
Ye Yint Kyaw <yeyint15852@gmail.com>
2024-04-26 11:41:22 +0700

    Updated

--- lib/screens/AdminTools/AdminTypes/SSSenior/sssenior_inputs.dart
+++ lib/screens/AdminTools/AdminTypes/SSSenior/sssenior_inputs.dart
@@ -1,4 +1,5 @@
 import 'package:champion_maung/constants.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 
 class SSSeniorInputsPage extends StatefulWidget {
@@ -9,6 +10,10 @@ class SSSeniorInputsPage extends StatefulWidget {
 }
 
 class _SSSeniorInputsPageState extends State<SSSeniorInputsPage> {
+  List<String> leagueList = [
+    'Premiere League',
+    'Spain Laliga',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,17 +30,134 @@ class _SSSeniorInputsPageState extends State<SSSeniorInputsPage> {
         ),
       ),
       body: Container(
+        width: double.infinity,
         color: kPrimary,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Expanded(
-                child: Container(),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      labelText('Select League'),
+                      Container(
+                        alignment: Alignment.topLeft,
+                        child: DropdownButtonHideUnderline(
+                          child: DropdownButton2<String>(
+                            isExpanded: true,
+                            hint: const Row(
+                              children: [
+                                Icon(
+                                  Icons.list,
+                                  size: 16,
+                                  color: kPrimary,
+                                ),
+                                SizedBox(
+                                  width: 4,
+                                ),
+                                Expanded(
+                                  child: Text(
+                                    'Select',
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                      color: kPrimary,
+                                    ),
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            items: leagueList
+                                .map((String item) => DropdownMenuItem<String>(
+                                      value: item,
+                                      child: Text(
+                                        item,
+                                        style: const TextStyle(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w600,
+                                          color: kPrimary,
+                                        ),
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                    ))
+                                .toList(),
+                            value: selectedValue,
+                            onChanged: (String? value) {
+                              setState(() {
+                                selectedValue = value;
+                              });
+                            },
+                            buttonStyleData: ButtonStyleData(
+                              height: 50,
+                              width: 160,
+                              padding:
+                                  const EdgeInsets.only(left: 14, right: 14),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(14),
+                                border: Border.all(
+                                  color: kBlue,
+                                ),
+                                color: kBlue,
+                              ),
+                              elevation: 2,
+                            ),
+                            iconStyleData: const IconStyleData(
+                              icon: Icon(
+                                Icons.arrow_forward_ios_outlined,
+                              ),
+                              iconSize: 14,
+                              iconEnabledColor: kPrimary,
+                              iconDisabledColor: kGrey,
+                            ),
+                            dropdownStyleData: DropdownStyleData(
+                              maxHeight: 200,
+                              width: 200,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(14),
+                                color: kBlue,
+                              ),
+                              offset: const Offset(-20, 0),
+                              scrollbarTheme: ScrollbarThemeData(
+                                radius: const Radius.circular(40),
+                                thickness: WidgetStateProperty.all<double>(6),
+                                thumbVisibility:
+                                    WidgetStateProperty.all<bool>(true),
+                              ),
+                            ),
+                            menuItemStyleData: const MenuItemStyleData(
+                              height: 40,
+                              padding: EdgeInsets.only(left: 14, right: 14),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 25.0),
+                      labelText('Home Team'),
+                      TextFormField(
+                        style: kTextFieldActiveStyle,
+                        decoration: kTextFieldDecoration.copyWith(
+                          hintText: 'Enter home team',
+                        ),
+                      ),
+                      SizedBox(height: 10.0),
+                      labelText('Away Team'),
+                      TextFormField(
+                        style: kTextFieldActiveStyle,
+                        decoration: kTextFieldDecoration.copyWith(
+                          hintText: 'Enter away team',
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
