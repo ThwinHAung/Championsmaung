5cd33b803f2edb7c6669bde7b859268d55e7ed0f
Ye Yint Kyaw <yeyint15852@gmail.com>
2024-04-28 17:00:55 +0700

    Updated

--- lib/screens/AdminTools/AdminTypes/SSSenior/sssenior_match_view.dart
+++ lib/screens/AdminTools/AdminTypes/SSSenior/sssenior_match_view.dart
@@ -12,17 +12,18 @@ class SSSeniorMatchView extends StatefulWidget {
 
 class _SSSeniorMatchViewState extends State<SSSeniorMatchView> {
   List<String> matchesList = ['1', '2'];
+  late String selectedRadio;
 
-  // Define variables to track the selected radio button and the corresponding container color
-  int _selectedValue = 0;
-  Color _containerColor = Colors.blue;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    selectedRadio = 'Option 1';
+  }
 
-  // Define a function to update the selected radio button and animate the container
-  void _handleRadioValueChanged(int value) {
+  setSelectedRadio(String val) {
     setState(() {
-      _selectedValue = value;
-      // Change container color based on selected radio button
-      _containerColor = value == 0 ? Colors.blue : Colors.red;
+      selectedRadio = val;
     });
   }
 
@@ -66,33 +67,13 @@ class _SSSeniorMatchViewState extends State<SSSeniorMatchView> {
                         padding: const EdgeInsets.all(8.0),
                         child: Container(
                           width: double.infinity,
-                          height: 100,
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(15),
                             color: kOnPrimaryContainer,
                           ),
                           child: Column(
                             mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              //Custom Radio Container
-                              CustomRadioContainer(
-                                  value: 1,
-                                  groupValue: _selectedValue,
-                                  onChanged: (value) {
-                                    setState(() {
-                                      _selectedValue = value;
-                                    });
-                                  }),
-                              SizedBox(height: 20.0),
-                              CustomRadioContainer(
-                                  value: 2,
-                                  groupValue: _selectedValue,
-                                  onChanged: (value) {
-                                    setState(() {
-                                      _selectedValue = value;
-                                    });
-                                  })
-                            ],
+                            children: <Widget>[],
                           ),
                         ),
                       ),
@@ -101,47 +82,6 @@ class _SSSeniorMatchViewState extends State<SSSeniorMatchView> {
                 );
               }),
         ),
-      ),
-    );
-  }
-}
-
-class CustomRadioContainer extends StatelessWidget {
-  final int value;
-  final int groupValue;
-  final ValueChanged<int> onChanged;
-
-  const CustomRadioContainer({
-    super.key,
-    required this.value,
-    required this.groupValue,
-    required this.onChanged,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        onChanged(value);
-      },
-      child: Container(
-        width: 50,
-        height: 50,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          border: Border.all(
-            color: groupValue == value ? Colors.blue : Colors.grey,
-            width: 2,
-          ),
-        ),
-        child: groupValue == value
-            ? Center(
-                child: Icon(
-                  Icons.check,
-                  color: Colors.blue,
-                ),
-              )
-            : null,
       ),
     );
   }
