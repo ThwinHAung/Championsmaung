f6fc4d57ceaa537501d00676e8490cb4f9f43728
Ye Yint Kyaw <yeyint15852@gmail.com>
2024-04-28 14:29:59 +0700

    Updated

--- lib/screens/AdminTools/AdminTypes/SSSenior/sssenior_match_view.dart
+++ lib/screens/AdminTools/AdminTypes/SSSenior/sssenior_match_view.dart
@@ -12,6 +12,20 @@ class SSSeniorMatchView extends StatefulWidget {
 
 class _SSSeniorMatchViewState extends State<SSSeniorMatchView> {
   List<String> matchesList = ['1', '2'];
+
+  // Define variables to track the selected radio button and the corresponding container color
+  int _selectedValue = 0;
+  Color _containerColor = Colors.blue;
+
+  // Define a function to update the selected radio button and animate the container
+  void _handleRadioValueChanged(int value) {
+    setState(() {
+      _selectedValue = value;
+      // Change container color based on selected radio button
+      _containerColor = value == 0 ? Colors.blue : Colors.red;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     double w = MediaQuery.of(context).size.width;
@@ -48,8 +62,39 @@ class _SSSeniorMatchViewState extends State<SSSeniorMatchView> {
                     child: FadeInAnimation(
                       curve: Curves.fastLinearToSlowEaseIn,
                       duration: const Duration(milliseconds: 2500),
-                      child: Container(
-                        child: matchCardView(),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          width: double.infinity,
+                          height: 100,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: kOnPrimaryContainer,
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              //Custom Radio Container
+                              CustomRadioContainer(
+                                  value: 1,
+                                  groupValue: _selectedValue,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      _selectedValue = value;
+                                    });
+                                  }),
+                              SizedBox(height: 20.0),
+                              CustomRadioContainer(
+                                  value: 2,
+                                  groupValue: _selectedValue,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      _selectedValue = value;
+                                    });
+                                  })
+                            ],
+                          ),
+                        ),
                       ),
                     ),
                   ),
@@ -59,17 +104,44 @@ class _SSSeniorMatchViewState extends State<SSSeniorMatchView> {
       ),
     );
   }
+}
 
-  Widget matchCardView() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
+class CustomRadioContainer extends StatelessWidget {
+  final int value;
+  final int groupValue;
+  final ValueChanged<int> onChanged;
+
+  const CustomRadioContainer({
+    super.key,
+    required this.value,
+    required this.groupValue,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        onChanged(value);
+      },
       child: Container(
-        width: double.infinity,
-        height: 100,
+        width: 50,
+        height: 50,
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          color: kOnPrimaryContainer,
+          shape: BoxShape.circle,
+          border: Border.all(
+            color: groupValue == value ? Colors.blue : Colors.grey,
+            width: 2,
+          ),
         ),
+        child: groupValue == value
+            ? Center(
+                child: Icon(
+                  Icons.check,
+                  color: Colors.blue,
+                ),
+              )
+            : null,
       ),
     );
   }
