41b6c6e199c845f983667e51d8906fe09811e9bd
ThwinHtooAung <htoohtoo.mdy.mmr@gmail.com>
2024-05-01 12:01:44 +0700

    a

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:champion_maung/constants.dart';
-import 'package:champion_maung/screens/AdminTools/AdminTypes/SSSenior/sssenior_match_view.dart';
+import 'package:champion_maung/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -53,8 +53,7 @@ class _SplashScreenState extends State<SplashScreen>
 
     Timer(const Duration(seconds: 4), () {
       setState(() {
-        Navigator.pushReplacement(
-            context, PageTransition(const SSSeniorMatchView()));
+        Navigator.pushReplacement(context, PageTransition(const LoginScreen()));
       });
     });
   }
