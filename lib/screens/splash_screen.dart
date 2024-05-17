52e15d982e7df5e4447ae0b8d03a28b0112f1ade
Ye Yint Kyaw <yeyint15852@gmail.com>
2024-05-17 12:06:44 +0700

    Updated

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:champion_maung/constants.dart';
+import 'package:champion_maung/screens/AdminTools/AdminTypes/SSSenior/sssenior_match_view.dart';
 import 'package:champion_maung/screens/AdminTools/AdminTypes/User/user_home_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -54,7 +55,7 @@ class _SplashScreenState extends State<SplashScreen>
     Timer(const Duration(seconds: 4), () {
       setState(() {
         Navigator.pushReplacement(
-            context, PageTransition(const UserHomeScreen()));
+            context, PageTransition(const SSSeniorMatchView()));
       });
     });
   }
